1d665b5eeb8497ea1c98ba5849e996deab18d20f
Amr Aboelela <amraboelela@gmail.com>
2022-01-25 13:35:36 -0800

    executable

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "ldbdump",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
+        .executable(
             name: "ldbdump",
             targets: ["ldbdump"]),
     ],
